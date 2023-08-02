b33a9ed7767c0d6c3b221f18cc2f621e46d5bc42
JamJubilo <jamermartinezg@gmail.com>
2023-08-01 21:09:12 -0500

    actualizaciones en modelo usuario
    actualizando y agregando roles, clases de documentos, enum

--- usuarios/models.py
+++ usuarios/models.py
@@ -4,14 +4,24 @@ from django.utils.translation import gettext_lazy as _
 # Create your models here.
 class Usuarios(models.Model):
     nombres = models.CharField(max_length=50,verbose_name="nombre")
-    apellidos = models.CharField(max_length=100)
-    tipo_documento = models.CharField(max_length=45)
+    apellidos = models.CharField(max_length=100, verbose_name="Apellido usuario")
     num_documento = models.IntegerField()
-    correo = models.CharField(max_length=45)
-    direccion = models.CharField(max_length=45)
+    correo = models.CharField(max_length=45, verbose_name="correo usuario")
+    direccion = models.CharField(max_length=45, verbose_name="Direccion usuario")
     num_telefono = models.IntegerField()
-    password = models.CharField(max_length=45)
-    rol = models.CharField(max_length=45)
+    password = models.CharField(max_length=45, verbose_name="Password usuario")
+    class TipoDocumento(models.TextChoices):
+        CC='C.C', _('Cédula de Ciudadanía')
+        CE='C.E', _('Cédula de Extranjería')
+        TI='T.I', _('Tarjeta de Identidad')
+        OT='Otro', _('Otro Tipo de Documento')
+    tipoDocumento=models.CharField(max_length=4, choices=TipoDocumento.choices, default=TipoDocumento.CC, verbose_name="Tipo de Documento")
+    class Rol(models.TextChoices):
+        Administrador='Administrador', _('Administrador')
+        Empleado='Empleado', _('Empleado')
+        Cliente='Cliente', _('Cliente')
+        Proveedor='Proveedor', _('Proveedor')
+    rol=models.CharField(max_length=13, choices=Rol.choices, default=Rol.Empleado, verbose_name="Rol")
     class Estado(models.TextChoices):
         ACTIVO='1', _('Activo') 
         INACTIVO= '0', _('Inactivo') 
