0400f9a93d14c683608980651f3d03c31cd63c0e
JamJubilo <jamermartinezg@gmail.com>
2023-08-03 09:28:07 -0500

    Modificación en modelos
    Eliminación de Apps estudiantes y docentes para integrar todas las clases un una sola App llamada Usuarios

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
-class Usuarios(models.Model):
+class Usuario(models.Model):
     nombres = models.CharField(max_length=50,verbose_name="nombre")
     apellidos = models.CharField(max_length=100, verbose_name="Apellido usuario")
     direccion = models.CharField(max_length=45, verbose_name="Direccion usuario")
@@ -21,13 +21,35 @@ class Usuarios(models.Model):
         Administrador='Administrador', _('Administrador')
         Estudiante='Estudiante', _('Estudiante')
         Docente='Docente', _('Docente')
-        
-        
     rol=models.CharField(max_length=13, choices=Rol.choices, default=Rol.Administrador, verbose_name="Rol")
     class Estado(models.TextChoices):
         ACTIVO='1', _('Activo') 
         INACTIVO= '0', _('Inactivo') 
     estado = models.CharField(max_length=1, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado") 
     def __str__(self):
-        return "%s %s" %(self.nombres, self.apellidos)   
+        return "%s %s" %(self.nombres, self.apellidos)  
+class Estudiante(models.Model):
+    Usuario_id_usuario = models.ForeignKey(Usuario,on_delete=models.PROTECT, verbose_name="Usuario", related_name="Usuario")
+    fecha_nacimiento = models.DateTimeField(verbose_name="fecha nacimiento del estudiante",help_text="DD/MM/AAAA")
+    factor_rh = models.CharField(max_length=20, verbose_name="Factor RH estudiante")
+    num_telefono = models.CharField(max_length=45,verbose_name="Numero de telfono del estudiante")
+    nom_acudiente = models.CharField(max_length=50,verbose_name="nombre acudiente de estudiante")
+    telefono_acudiente = models.CharField(max_length=45,verbose_name="Numero de telefono del acudiente") 
     
+    class Estado(models.TextChoices):
+        ACTIVO='1', _('Activo') 
+        INACTIVO= '0', _('Inactivo') 
+    estado = models.CharField(max_length=1, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado")  
+    
+class Docente(models.Model):
+    Usuario_id_usuario = models.ForeignKey(Usuario,on_delete=models.PROTECT, verbose_name="usuario", related_name="usuario")
+    asignatura_imparte = models.CharField(max_length=45, verbose_name="Asignatura que imparte docente")
+
+    class Estado(models.TextChoices):
+        ACTIVO='1', _('Activo') 
+        INACTIVO= '0', _('Inactivo') 
+    estado = models.CharField(max_length=1, choices=Estado.choices, default=Estado.ACTIVO, verbose_name="Estado")
+    
+class Titulos(models.Model):
+    Docente_id_Docente = models.ForeignKey(Docente,on_delete=models.PROTECT,verbose_name="tiutlo del docente")  
+    nombre_titulo = models.CharField(max_length=45,verbose_name="Titulo del docente")
