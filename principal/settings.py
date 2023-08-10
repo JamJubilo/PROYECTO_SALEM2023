6b09b9ecd52b1c61d77f380971fcd427bcd26eae
JamJubilo <jamermartinezg@gmail.com>
2023-08-09 22:00:51 -0500

    creación partials
    se crearon body, footer, header ,main dentro de partials

--- principal/settings.py
+++ principal/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'asignaturas',
     'usuarios',
     'grados',  
-    
+    'bootstrap5', # pip install django-bootstrap-v5
 ]
 
 MIDDLEWARE = [
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT= "/static"
 
 MEDIA_URL= '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
